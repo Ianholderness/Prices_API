ff5aa57c64606cacd3052028a2caea71fb84a02d
Ianholderness <68484796+Ianholderness@users.noreply.github.com>
2021-07-28 21:47:03 +0100

    Update BMRA API.py
    Added SBP / SSP

--- BMRA API.py
+++ BMRA API.py
@@ -1,23 +1,52 @@
 import os 
 import sys
 import requests
-from pprint import pprint
 import json
 
-#if __name__ == "__main__":
+def GetWeighted_Price(url):
+    #outputs the url to see date and HH requested
+    #print("Sending request "+ " "+ url)
+    #sends a get request to collect the data and format the data into Json arr
+    responce = requests.get(url).json()
+    # split the arry
+    data = responce['arr']
+    #Calculate the volume and MWH for APX and N2EX
+    nVol = (float(data[0]['MarketIndexPrice']) * float(data[0]['marketIndexVolume']))+(float(data[1]['MarketIndexPrice']) * float(data[1]['marketIndexVolume']))
+    nMwh = float(data[0]['marketIndexVolume']) + float(data[1]['marketIndexVolume'])
+    #Print the result
 
-#url = 'https://bmreports.com/bmrs/?q=ajax/alldata/MID/Date,SP,Provider,Price,Volume/null/'
+    print(round(nVol / nMwh, 4))
+    #return round(nVol / nMwh, 4)
 
-#    sFile_Path = sys.argv[1]
-#    sDate = sys.argv[2]
+def Check_SSP_Price(url, HH):
+    #outputs the url to see date and HH requested
+    #print("Sending request "+ " "+ url)
+    #sends a get request to collect the data and format the data into Json arr
+    responce = requests.get(url).json()
+    # split the arry
+    data = responce['arr']
+    #print(data)
 
-url = 'https://bmreports.com/bmrs/?q=ajax/alldata/MID/Date,SP,Provider,Price,Volume/null/2021-06-12/7'
+    for i in data:
+        if i['sp'] == HH:
+            print("SSP :" + i['ssp'] + " SBP : " + i['sbp'])
+            break
 
-print("Sending request "+ " "+ url)
-#responce = requests.Get(url)
-#return responce.json()
+#Code starts from here
 
-#print(response)
+if __name__ == "__main__":
 
-#response_json = response.json()
-#pprint(response_json)
+    pPrice = sys.argv[1] #Price
+    pdate = sys.argv[2] #Date
+    phh = sys.argv[3] #HH
+
+    if pPrice == 'APX':
+
+        #Concates the prmas into the url
+        url = 'https://bmreports.com/bmrs/?q=ajax/alldata/MID/Date,SP,Provider,Price,Volume/null/'+ pdate + "/" + phh
+        #Calls the function to get the data
+        GetWeighted_Price(url)
+
+    if pPrice == 'SSP':
+        url = 'https://bmreports.com/bmrs/?q=ajax/alldata/DERSYSDATA/settlementDate,Period,SSP,SBP,BD,PDC,RSP,NIV,SPA,BPA,RP,RPRV,OV,BV,TOV,TBV,ASV,ABV,TASV,TABV,Linkstodata/NULL/'+ pdate
+        Check_SSP_Price(url, phh)
